d9fcc2961bddadbfc0910ff29c184c7a3b0a1424
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2024-09-10 19:25:53 +0530

    encryption program

---
+++ encryption.py
@@ -0,0 +1,18 @@
+import random 
+import string
+
+chars = "  " +  string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+text = input("Enter a message to encrypt: ")
+encrypt = ""
+
+for letter in text:
+    index = chars.index(letter)
+    encrypt += key[index] 
+
+print(f'original message: {text}')
+print(f"encrypted message: {encrypt}")
